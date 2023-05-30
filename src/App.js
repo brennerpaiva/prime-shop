c0c4b3b2b72fa41f7871572edeba66c98d5323eb
unknown <brennerpaiva@gmail.com>
2023-05-30 14:53:35 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="app"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
